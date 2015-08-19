19c42b6808578fb0cd19d01829abf2b3f829ee6f
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2015-08-19 17:15:09 -0400

    Atividade 5 Aula 5 - Adicionando a classe esconde ao botão via interação do usuário

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,14 @@
 document.querySelector('.menu-abrir').onclick = function() {
     document.documentElement.classList.add('menu-ativo');
+    document.querySelector('.menu-abrir').classList.add('esconde');
 };
 
 document.querySelector('.menu-fechar').onclick = function() {
     document.documentElement.classList.remove('menu-ativo');
+    document.querySelector('.menu-abrir').classList.remove('esconde');
+};
+document.documentElement.onclick = function(event) {
+    if (event.target === document.documentElement) {
+        document.documentElement.classList.remove('menu-ativo');
+    }
 };
